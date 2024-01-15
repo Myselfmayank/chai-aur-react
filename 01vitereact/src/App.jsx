4fefde3a3cc1c6b3522ac18a468fe68f4ca6db61
Mayank Raj <mmaayankraj@gmail.com>
2024-01-16 00:58:36 +0530

    working of react library-behind the scenes

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,14 +1,16 @@
 import Chai from './Chai'
 
 function App() {
-
+    //variable to use in react
+    //only evaluated exppression can be used in react
+    const userName = "chai aur code with Mayank";
 
   return (
     // only one html element can be returned at a time.
     // we can use fragments to return multiple elements <> </>
     <>
     <Chai/>
-    <h1>chai aur react</h1>
+    <h1>chai aur react {userName}</h1>
     <p>Test para</p>
     </>
     
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+    return (
+        <div>
+            <h1>Custom App!</h1>
+        </div>
+    )
+}
+
+// Js object containing html element which can be rendered just by mentioning name in render method
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
+const anotherUser = 'Mayank'
+
+//pre provided method by React
+//Babble inject this method into DOM
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com',target: '_blank'},
+    'click me to visit google',
+    anotherUser
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    //reactElement
+    <App/>
 )
