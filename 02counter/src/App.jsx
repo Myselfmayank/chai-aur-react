4c2e89f5114d23f9eb426eff2bc3cb75ee4448a9
Mayank Raj <mmaayankraj@gmail.com>
2024-01-21 22:18:34 +0530

    React Interview Question on Counter

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -12,8 +12,17 @@ function App() {
     //let counter = 5;
     const addValue = () =>{
         //counter = counter + 1;
-        if(counter <= 19)
-        setCounter(counter+1);
+        if(counter <= 19){
+            //Even after calling setCounter multiple times only one value will be increased at a time.
+            // Because react send UI updates in batches and ignore duplicate operations
+            // To resolve this
+            //setCounter(counter+1);
+            setCounter(prevCounter => counter + 1)
+            setCounter(prevCounter => counter + 1)
+            setCounter(prevCounter => counter + 1)
+            setCounter(prevCounter => counter + 1)
+        }
+        
     }
 
     const decreaseValue = () =>{
