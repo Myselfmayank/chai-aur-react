1f467f6f8650d48fd8eeb7f9aa503e8ccd5d6025
Mayank Raj <mmaayankraj@gmail.com>
2024-02-12 02:44:47 +0530

    added a comment in main.jsx in reactRouter

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -40,7 +40,7 @@ const router1 = createBrowserRouter(
             <Route path='about' element={<About/>}/>
             <Route path='contact' element={<Contact/>}/>
             <Route path='user/:userid' element={<User/>}/>
-            <Route loader={githubInfoLoader} path='github' element={<Github/>}/>
+            <Route loader={githubInfoLoader} path='github' element={<Github/>}/> //loader is used to fetch data from api beforehand to avoid lag
         </Route>
     )
 )
